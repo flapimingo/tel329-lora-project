4f3102d49d7501c353393e3420410739230fe330
Pascal Arriagada <pascal.arriagada@gmail.com>
2021-12-19 13:07:30 -0300

    added sd card writing operation

--- node/node.ino
+++ node/node.ino
@@ -1,69 +1,63 @@
-/* Arduino example code for DHT11, DHT22/AM2302 and DHT21/AM2301 temperature and humidity sensors. More info: www.www.makerguides.com */
+#include "SD.h"
+#include <SPI.h>
+#include "DHT.h" 
 
-// Include the libraries:
-#include <Adafruit_Sensor.h>
-#include <DHT.h>
+ #define DHTPIN 2     
+ #define DHTTYPE DHT22
+ DHT dht (DHTPIN, DHTTYPE) ;
 
-// Set DHT pin:
-#define DHTPIN 2
-
-// Set DHT type, uncomment whatever type you're using!
-//#define DHTTYPE DHT11   // DHT 11 
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
-
-// Initialize DHT sensor for normal 16mhz Arduino:
-DHT dht = DHT(DHTPIN, DHTTYPE);
+File myFile;
+String Temperature, Humidity, Data;
 
 void setup() {
-  // Begin serial communication at a baud rate of 9600:
-  Serial.begin(9600);
+  Serial.begin(115200);
+  dht.begin (  ) ;  
 
-  // Setup sensor:
-  dht.begin();
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(10)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");
+  myFile = SD.open("data.txt", FILE_WRITE);
+  if (myFile) {
+    myFile.println( "Temperature(°C), Humidity(%) \r\n");
+    myFile.close();
+  } 
+  else {
+    Serial.println("error opening data.txt");
+  }
 }
 
 void loop() {
-  // Wait a few seconds between measurements:
-  delay(2000);
+   if ( isnan (dht.readTemperature ( ) ) || isnan (dht.readHumidity ( ) ) )
+   {
+     Serial.println ("DHT22 Sensor not working!") ;
+   }
+ else
+{   
+  data_logging();                                                 
+ }
+delay(20000); 
+}
 
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-
-  // Read the humidity in %:
-  float h = dht.readHumidity();
-  // Read the temperature as Celsius:
-  float t = dht.readTemperature();
-  // Read the temperature as Fahrenheit:
-  float f = dht.readTemperature(true);
-
-  // Check if any reads failed and exit early (to try again):
-  if (isnan(h) || isnan(t) || isnan(f)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
-
-  // Compute heat index in Fahrenheit (default):
-  float hif = dht.computeHeatIndex(f, h);
-  // Compute heat index in Celsius:
-  float hic = dht.computeHeatIndex(t, h, false);
-
-
-  Serial.print("Humidity: ");
-  Serial.print(h);
-  Serial.print(" % ");
-  Serial.print("Temperature: ");
-  Serial.print(t);
-  Serial.print(" \xC2\xB0");
-  Serial.print("C | ");
-  Serial.print(f);
-  Serial.print(" \xC2\xB0");
-  Serial.print("F ");
-  Serial.print("Heat index: ");
-  Serial.print(hic);
-  Serial.print(" \xC2\xB0");
-  Serial.print("C | ");
-  Serial.print(hif);
-  Serial.print(" \xC2\xB0");
-  Serial.println("F");
+void data_logging() 
+{
+  String Temperature = String(dht.readTemperature ( ), 2);
+  String Humidity = String(dht.readHumidity ( ), 2);
+  Data = Temperature + "," + Humidity;
+  
+  Serial.print("Save data: ");
+  Serial.println(Data);
+  
+  myFile = SD.open("data.txt", FILE_WRITE);
+  if (myFile) {
+    Serial.print("Writing to data.txt...");
+    myFile.println(Data);
+    myFile.close();
+    Serial.println("done.");
+  } else {
+    Serial.println("error opening data.txt");
+  }  
+  Serial.println();
 }
