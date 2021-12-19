fed7cb114d7d5271cab541beee4903879c9a6556
Pascal Arriagada <pascal.arriagada@gmail.com>
2021-12-19 13:08:12 -0300

    added RTC module for timestamp data logging

--- node/node.ino
+++ node/node.ino
@@ -1,25 +1,53 @@
-#include "SD.h"
-#include <SPI.h>
-#include "DHT.h" 
+#include <SPI.h> //for the SD card module
+#include <SD.h> // for the SD card
+#include <DHT.h> // for the DHT sensor
+#include <RTClib.h> // for the RTC
 
- #define DHTPIN 2     
- #define DHTTYPE DHT22
- DHT dht (DHTPIN, DHTTYPE) ;
+//define DHT pin
+#define DHTPIN 3     
+#define DHTTYPE DHT22
 
+// initialize DHT sensor for normal 16mhz Arduino
+DHT dht (DHTPIN, DHTTYPE);
+
+const int chipSelect = 10; 
+
+// Create a file to store the data
 File myFile;
 String Temperature, Humidity, Data;
 
-void setup() {
-  Serial.begin(115200);
-  dht.begin (  ) ;  
+// RTC
+RTC_DS1307 rtc;
 
+void setup() {
+  Serial.begin(9600);
+  // Initializing the DHT sensor
+  dht.begin() ;  
+
+  while(!Serial); // for Leonardo/Micro/Zero
+  if(! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+  else {
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+  if(! rtc.isrunning()) {
+    Serial.println("RTC is NOT running!");
+  }
+
+  // Setup for the SD card
   Serial.print("Initializing SD card...");
-  if (!SD.begin(10)) {
+  if (!SD.begin(chipSelect)) {
     Serial.println("initialization failed!");
     while (1);
   }
   Serial.println("initialization done.");
+
+  // Open file
   myFile = SD.open("data.txt", FILE_WRITE);
+  // If its opened, write to it
   if (myFile) {
     myFile.println( "Temperature(°C), Humidity(%) \r\n");
     myFile.close();
@@ -29,35 +57,69 @@ void setup() {
   }
 }
 
-void loop() {
-   if ( isnan (dht.readTemperature ( ) ) || isnan (dht.readHumidity ( ) ) )
-   {
-     Serial.println ("DHT22 Sensor not working!") ;
-   }
- else
-{   
-  data_logging();                                                 
- }
-delay(20000); 
+void loggingTime() {
+  DateTime now = rtc.now();
+  myFile = SD.open("DATA.txt", FILE_WRITE);
+  if (myFile) {
+    myFile.print(now.year(), DEC);
+    myFile.print('/');
+    myFile.print(now.month(), DEC);
+    myFile.print('/');
+    myFile.print(now.day(), DEC);
+    myFile.print(',');
+    myFile.print(now.hour(), DEC);
+    myFile.print(':');
+    myFile.print(now.minute(), DEC);
+    myFile.print(':');
+    myFile.print(now.second(), DEC);
+    myFile.print(",");
+  }
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.println(now.day(), DEC);
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.println(now.second(), DEC);
+  myFile.close();
+  delay(1000);  
 }
 
-void data_logging() 
-{
-  String Temperature = String(dht.readTemperature ( ), 2);
-  String Humidity = String(dht.readHumidity ( ), 2);
-  Data = Temperature + "," + Humidity;
+void loggingTemperature() {
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  // Read temperature as Celsius
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit
+  //float f = dht.readTemperature(true);
   
-  Serial.print("Save data: ");
-  Serial.println(Data);
+  // Check if any reads failed and exit early (to try again).
+  if  (isnan(t) /*|| isnan(f)*/) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
   
-  myFile = SD.open("data.txt", FILE_WRITE);
+  //debugging purposes
+  Serial.print("Temperature: "); 
+  Serial.print(t);
+  Serial.println(" *C");
+  //Serial.print(f);
+  //Serial.println(" *F\t"); 
+  
+  myFile = SD.open("DATA.txt", FILE_WRITE);
   if (myFile) {
-    Serial.print("Writing to data.txt...");
-    myFile.println(Data);
-    myFile.close();
-    Serial.println("done.");
-  } else {
-    Serial.println("error opening data.txt");
-  }  
-  Serial.println();
+    Serial.println("open with success");
+    myFile.print(t);
+    myFile.println(",");
+  }
+  myFile.close();
+}
+
+void loop() {
+  loggingTime();
+  loggingTemperature();
+  delay(5000);
 }
